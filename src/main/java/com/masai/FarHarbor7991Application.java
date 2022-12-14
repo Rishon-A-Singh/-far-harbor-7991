82f1cc64d57dfe3a865c265b2bd7f42dba808a71
Rishon-A-Singh <rishon.singh@gmail.com>
2022-12-14 12:22:43 +0530

    initial commit

---
+++ src/main/java/com/masai/FarHarbor7991Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarHarbor7991Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarHarbor7991Application.class, args);
+	}
+
+}
